7b06a5407d76b697396caa54d14e6b7963406a1b
Joshua Spence <josh@freelancer.com>
2015-09-04 19:06:12 +1000

    Recognize `joshua.james.spence@gmail.com` as being me

--- filters.rb
+++ filters.rb
@@ -1,10 +1,14 @@
 require 'gmail-britta'
 
-fs = GmailBritta.filterset(:me => ['josh@joshuaspence.com',
-                                   'joshua@joshuaspence.com',
-                                   'josh@joshuaspence.com.au',
-                                   'joshua@joshuaspence.com.au',
-                                  ]) do
+
+fs = GmailBritta.filterset(:me => [
+  'josh@joshuaspence.com',
+  'joshua@joshuaspence.com',
+  'josh@joshuaspence.com.au',
+  'joshua@joshuaspence.com.au',
+  'joshua.james.spence@gmail.com',
+]) do
+
   # Employment: Howard and Sons
   filter {
     has ['from:howardsfireworks.com.au']
